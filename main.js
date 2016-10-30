1b66bd7dbf0839b8c44926b188253336f1b89b1a
Shelikhoo <xiaokangwang@outlook.com>
2016-10-30 22:40:56 +0800

    msg

--- main.js
+++ main.js
@@ -13,8 +13,11 @@ let mainWindow;
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1200,
+        height: 800,
+        'web-preferences': {
+            'web-security': false
+        }
     });
 
     // and load the index.html of the app.
--- main_page.jsx
+++ main_page.jsx
@@ -1,6 +1,6 @@
 /*jshint browser: true*/
 /*jshint esversion: 6 */
-/*global $, jQuery, alert, ReactDOM, React*/ 
+/*global $, jQuery, alert, ReactDOM, React*/
 class Header extends React.Component {
     render(){
         return (
@@ -15,21 +15,55 @@ class CurrentCard extends React.Component {
     render(){
         return (<div>
             <div className="jumbotron">
-                <p className="annou">X等奖，第Y位</p>
-                <h1 className="currentnum">123</h1>
+                <p className="annou">{this.props.currentTier}等奖，第{this.props.currentNo}位:</p>
+                <h1 className="currentnum">{this.props.currentNo}</h1>
             </div>
             </div>
         );
     }
 }
+
+class HistoryCard extends React.Component {
+    render(){
+        return (<div>
+                <div className="card">
+                        <div className="card-block">
+                            {this.props.prizeTier}等奖:{this.props.prizeGoneTo}
+                        </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 class HistoryCardList extends React.Component {
     render(){
-        return (<div></div>
+        return (<div>
+                <HistoryCard prizeTier="3" prizeGoneTo={this.props.history[0]}></HistoryCard>
+                <HistoryCard prizeTier="3" prizeGoneTo={this.props.history[1]}></HistoryCard>
+                <HistoryCard prizeTier="3" prizeGoneTo={this.props.history[2]}></HistoryCard>
+                <HistoryCard prizeTier="2" prizeGoneTo={this.props.history[3]}></HistoryCard>
+                <HistoryCard prizeTier="2" prizeGoneTo={this.props.history[4]}></HistoryCard>
+            </div>
         );
     }
 }
 class MainContent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+                history : {
+                    prizes: Array(6).fill("***")
+                },
+                currentNo:0
+        };
+    }
+    handleMessage(e){
+      console.log(e.data);
+    }
     render(){
+        this.props.tern.onmessage= e => {this.handleMessage(e)} ;
+        console.log(mc.port1);
         return (
             <div>
                 <div className="row">
@@ -37,7 +71,7 @@ class MainContent extends React.Component {
                         <CurrentCard></CurrentCard>
                     </div>
                     <div className="col-sm-4">
-                        <HistoryCardList></HistoryCardList>
+                        <HistoryCardList history={this.state.history.prizes}></HistoryCardList>
                     </div>
                 </div>
             </div>
@@ -50,15 +84,15 @@ class Page extends React.Component {
         return (
             <div className="container">
                 <Header></Header>
-                <MainContent></MainContent>
+                <MainContent tern={this.props.tern}></MainContent>
             </div>
         );
     }
 }
-const mc = new MessageChannel();
+var mc = new MessageChannel();
 ReactDOM.render(
         <Page tern={mc.port1}/>,
         $("#reactroot")[0]
     );
 
-const mcsender=mc.port2;
+var mcsender=mc.port2;
