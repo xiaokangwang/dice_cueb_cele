c3e242ab76e82b9afbb82504348b61340ae5a06b
Shelikhoo <xiaokangwang@outlook.com>
2016-10-31 16:41:11 +0800

    speeding up flash

--- renderer.js
+++ renderer.js
@@ -2,33 +2,40 @@
 /*global $, jQuery, alert*/
 (function() {
     "use strict";
+
     function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
     window.tf = () => {
         console.log("x");
     };
     window.logicm = class Logic {
         constructor() {
             this.rant = [3, 3, 3, 2, 2, 1];
-            this.prizeSummonDistribution=[
-                  [0, 50], [50, 100], [100, 150], [150, 200], [200, 250], [250, 300]];
+            this.prizeSummonDistribution = [
+                [0, 50],
+                [50, 100],
+                [100, 150],
+                [150, 200],
+                [200, 250],
+                [250, 300]
+            ];
             this.flashtmp = 2;
             this.laststarttime = 0; /* second*1000 */
             this.laststartrank = 0;
-            this.flashCurrentCounter=0;
+            this.flashCurrentCounter = 0;
         }
         noToTier(no) {
             return this.rant[no];
         }
         getFlashNoS(no) {
-            let ranges=this.prizeSummonDistribution[no];
-            return getRandomInt(ranges[0],ranges[1]);
+            let ranges = this.prizeSummonDistribution[no];
+            return getRandomInt(ranges[0], ranges[1]);
         }
         FlashStart(no) {
             this.laststarttime = new Date().getTime();
             this.laststartrank = no;
-            this.flashCurrentCounter=0;
+            this.flashCurrentCounter = 0;
             return true;
         }
         isFlashShouldKeep() {
@@ -38,8 +45,8 @@
             return true;
         }
         getFlashInterval() {
-            this.flashCurrentCounter++
-            let calcs =  60-11*Math.log(this.flashCurrentCounter);
+            this.flashCurrentCounter++;
+            let calcs = 2*(60 - 13 * Math.log(this.flashCurrentCounter));
             return calcs;
         }
         getNextWinner() {
